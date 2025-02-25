07ef3f4f3c259ff4c43f06c4e5288d1ae4a53fd4
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-02-25 23:52:15 +0900

    add pseudo style

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,14 @@ function App() {
     <>
       <div className="container">
         <h1 className="text-6xl">Tailwind CSS Playground</h1>
+        <p>
+          <a
+            href="/"
+            className="text-blue-500 hover:opacity-60 hover:underline"
+          >
+            Sample Link
+          </a>
+        </p>
       </div>
     </>
   )
